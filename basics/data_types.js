6326e85edf3d4ea594ac8a2fe5e388b376f72305
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-04-02 19:53:52 +0000

    updated

---
+++ basics/data_types.js
@@ -0,0 +1,13 @@
+//String ,Number,Boolean,null,undefined,Symbol,BigInt
+
+// reference type
+// Array,Objects,Functions
+let userEmail;
+const hero=["thor","hulk","deadpool"]
+letmyobj={
+    name:"hitesh",
+    age:22,
+}
+const myFunction=function(){
+    console.log("hello World");
+}
