27e35f2ad794ad83335b95cfe6f0e8533cf8069d
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-04-02 20:25:08 +0000

    updated

---
+++ basics/vid10.js
@@ -0,0 +1,10 @@
+// Stack Memory primitive type
+// non primitive heap
+
+let myYoutubename="something";
+let another_name=myYoutubename;
+console.log(myYoutubename);
+console.log(another_name)
+another_name="anything";
+console.log("after changing");
+console.log(another_name);//
