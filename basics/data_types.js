312fc8bc7366eb0dc0d690602bb59b63dc6b5d53
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-04-02 20:14:02 +0000

    updated

--- basics/data_types.js
+++ basics/data_types.js
@@ -4,10 +4,14 @@
 // Array,Objects,Functions
 let userEmail;
 const hero=["thor","hulk","deadpool"]
-letmyobj={
-    name:"hitesh",
-    age:22,
+let myobj={
+    name:"aritra",
+    age:21,
 }
 const myFunction=function(){
     console.log("hello World");
 }
+//typeof() in javascript
+console.log(typeof(myFunction))// object function
+console.log(hero[0])
+console.log(myobj)
