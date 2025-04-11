94deae2807f0ade47799ed0d17d24f33ad534467
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-04-11 17:05:06 +0000

    updated

---
+++ basics2/objectsLast.js
@@ -0,0 +1,22 @@
+const course={
+    coursename:"js in hindi",
+    prince:"999",
+    course:"dbms"
+
+}
+const{courseInstructor: instructor}=course
+console.log(instructor)
+// const navbar=({})=>{
+    
+// }
+// JSON java script object notation
+// {
+//     name:"sam",
+//     "coursename":"js in hindi",
+//     "price":3000,
+// };
+// [
+//     {},
+//     {},
+//     {}
+// ]
