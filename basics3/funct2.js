b4ab61d8491c100cd9e4cfd24e956af144d2a004
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-04-17 19:48:08 +0000

    updated

---
+++ basics3/funct2.js
@@ -0,0 +1,15 @@
+//global scope
+//var c=40
+let a=90;
+// if(true){
+//     let a=10;
+//     const b=20;
+//     var c=30;// var is mostly avoid
+//     console.log("block",a);
+// }
+// console.log("global",a);
+// console.log(b);
+//console.log(c);
+for(let i=0;i<array.length;i++){
+    const ele=array[i];
+}
