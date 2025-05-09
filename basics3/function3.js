46dcbc643e3c2c24f3d7ceb72f7b4897d9783fc3
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-09 19:48:52 +0000

    complete

---
+++ basics3/function3.js
@@ -0,0 +1,28 @@
+function one(){
+  const username="hitesh"
+  function two(){
+    const website="youtube"
+    console.log(username);
+
+  } //console.log(website)
+  two ()
+}
+//one()
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website=" youtube"
+        console.log(username+website)
+    }
+
+}
+//console.log(username)
+//important part
+function addone(num){
+    return num+1
+}
+//addTwo(5)
+const addTwo=function(num){
+    return num+2
+}
+addTwo(4)
