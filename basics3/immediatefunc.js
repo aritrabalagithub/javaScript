ac603a767c0c2e1341d2bbe10362e52a20663b3b
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-10 07:08:10 +0000

    connected

---
+++ basics3/immediatefunc.js
@@ -0,0 +1,9 @@
+(function chai(){
+    console.log(`DB connected`)
+})();
+(function chai(){
+    console.log(`server starting`);
+})();
+((name)=>{
+    console.log(`DB CONNECTED TWO ${name}`);
+})('name')
