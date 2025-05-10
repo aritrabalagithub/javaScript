adfb38cadad83e22d42f45ba59065fb450bca255
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-10 06:57:03 +0000

    complete

---
+++ basics3/arrowfunction.js
@@ -0,0 +1,20 @@
+// function chai(){
+//     let username="hitesh"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai= ()=>{
+//     let username="hitesh"
+//     console.log(this.username)
+// }
+//()=>{}
+    // const addTwo=(num1,num2)=>{
+    //     return num1+num2
+    // }
+//const addTwo=(num1,num2)=>num1+num2
+//if curly braces return must if () used then no need of return
+ const addTwo=(num1,num2)=>({username:"hitesh"})
+ console.log(addTwo(5,6))
+ const myArr=[2,5,6,9]
+ //MimeTypeArray.forEach(()=>{})
--- basics3/function4.js
+++ basics3/function4.js
@@ -3,9 +3,10 @@ const user={
     price:999,
      welcomeMessage: function(){
         //console.log(`${this.username},welcome to website`);
-        console.log(this)
+       // console.log(this)
     }
 }
 user.welcomeMessage()
 user.username="sam"
 user.welcomeMessage()
+console.log(this)
