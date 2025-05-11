ca76b65855b40c8f29a03f24f18cb14f900be55c
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-11 18:21:10 +0000

    comm

---
+++ controlflow/switch.js
@@ -0,0 +1,27 @@
+// const month=3
+// switch(month){
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("mar");
+//         break;
+//     default:
+//         console.log("not in the range");
+// }
+ const month="str"
+switch(month){   case "jan":
+        console.log("january");
+         break;
+     case "feb":
+         console.log("february");
+         break;
+     case "mar":
+         console.log("march");
+         break;     
+        default:
+        console.log("not in the range");
+}
