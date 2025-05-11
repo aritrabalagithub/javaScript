b2c5dee3862c546ae776c5c4380326ed54aaa5a6
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-11 19:01:31 +0000

    complete

---
+++ controlflow/truly.js
@@ -0,0 +1,21 @@
+// const userEmail="hit@290"
+// if(userEmail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("dont have user email");
+// }
+//false,0,-0,BigInt 0n,null,undefined,NaN
+//true ,"0",'false'," ",[],{},function(){},
+// const emptyObj={}
+// if(Object.keys(emptyObj).lenght===0){
+//     console.log("object is empty");
+
+// }
+//nullish coalescencing operator
+let val1;
+//val1=null??10
+// val1=null??10??20
+// console.log(val1)
+let icetea=100;
+icetea>=80?console.log("more than 80"):console.log("less than 80");
