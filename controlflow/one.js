3bd18f1a34600f4cc8e55f09ace568c845b80855
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-11 16:38:54 +0000

    complete

---
+++ controlflow/one.js
@@ -0,0 +1,26 @@
+//<,>,<=,>=,==,!=
+// const isUserLoggedIn=true
+// if(2=="2"){
+// console.log("executed")
+// }
+//const score=200;
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power ${power}`);
+// }
+const balance=1000
+if(balance<500){
+    console.log("less than")
+}
+else if(balance<750){
+    console.log("less than 750")
+
+}
+else{
+    console.log("less than 1200")
+}
+const userLoggedIn=true
+const debitCard=true
+if(userLoggedIn&& debitCard && 2==3){
+    console.log("allow to buy course")
+}
