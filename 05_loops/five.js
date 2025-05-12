a10658a8dd47f64314ad6ea518d814329f58b746
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-12 04:52:24 +0000

    complete

---
+++ 05_loops/five.js
@@ -0,0 +1,28 @@
+const programming=['js','cpp','ruby','swift']
+// programming.forEach(function (val){
+// console.log(val)
+// })
+//  programming.forEach((item)=>{
+//      console.log(item)
+//  })
+//  function printMe(item){
+//      console.log(item);
+//  }
+// programming.forEach(printMe)
+// programming.forEach((item,index,arr)=>{
+// console.log(item,index,arr);
+// })
+const myCoding=[{
+    languageName:"javaScript",
+    languageFileName:"js"
+},{
+    languageName:"java",
+    languageFileName:"java"
+},{
+    languageName:"python",
+    languageFileName:"py"
+}
+]
+myCoding.forEach((item)=>{
+    console.log(item)
+})
