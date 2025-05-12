945e64255b9e748ea58dfb1c38062817e3897e0c
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-12 04:02:29 +0000

    commit

--- 05_loops/three.js
+++ 05_loops/three.js
@@ -3,7 +3,15 @@
 // for(const num of arr){
 // console.log(num);
 // }
-const greetings="hello world!"
-for(const greet of greetings){
-    console.log(`char is${greet}`);
-}
+// const greetings="hello world!"
+// for(const greet of greetings){
+//     console.log(`char is${greet}`);
+// }
+// const map=new Map();
+// map.set('IN',"INDIA");
+// map.set('US',"UNITED STATES");
+// map.set('PO',"POLAND")
+//   for(const [key,value] of map){
+//     console.log(value);
+//   }
+  
