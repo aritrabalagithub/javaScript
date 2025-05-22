f6f0eeb55a5dc76e268796a91afbd828188bf60a
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-22 07:02:12 +0000

    updated

--- 10_classes_and_oops/Objects.js
+++ 10_classes_and_oops/Objects.js
@@ -5,3 +5,7 @@ multipleBy5.power=2;
 console.log(multipleBy5.power);
 console.log(multipleBy5.prototype);
 console.log(multipleBy5(5));
+function createUser(username,score){
+    this.username=username;
+    this.score=score;
+}
