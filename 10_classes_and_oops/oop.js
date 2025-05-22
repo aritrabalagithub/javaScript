6d27ab1545800f6d417c156310726a4cbccdc89f
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-22 04:29:57 +0000

    update

--- 10_classes_and_oops/oop.js
+++ 10_classes_and_oops/oop.js
@@ -1,6 +1,12 @@
 const user={
     username:"hitesh",
     loginCount:8,
-    signedIn:true
+    signedIn:true,
+    
+    getUserDetails:function(){
+        //console.log(`username:${this.username}`);
+        console.log(this);
+    }
 }
 console.log(user.username);
+console.log(user.getUserDetails());
