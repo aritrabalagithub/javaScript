6ce5c6248169d0894456ba3d3ca971c1be6afc8d
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-22 13:56:26 +0000

    new

--- 10_classes_and_oops/myClasses.js
+++ 10_classes_and_oops/myClasses.js
@@ -4,13 +4,20 @@ constructor(username,email,password){
     this.email=email;
     this.password=password;
 }
-encryptPassword(){
-    return `${this.password}abc`
+// encryptPassword(){
+//     return `${this.password}abc`
+// }
+//  Changeusername(){
+//     return`${this.username.toUpperCase()}`
+//  }
 }
- Changeusername(){
-    return`${this.username.toUpperCase()}`
- }
+User.prototype.Changeusername=function(){
+    return`${this.username.toUpperCase()}`;
+}
+User.prototype.encryptPassword=function(){
+    return `${this.password}abc&`;
 }
 const chai=new User("user1","user1@chai",17829);
+
 console.log(chai.encryptPassword());
 console.log(chai.Changeusername())
