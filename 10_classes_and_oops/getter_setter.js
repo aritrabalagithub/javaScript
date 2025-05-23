acac1f6b91b042c7b1d30978a08773208cba7548
aritrabalagithub <121689777+aritrabalagithub@users.noreply.github.com>
2025-05-23 05:13:15 +0000

    updated

--- 10_classes_and_oops/getter_setter.js
+++ 10_classes_and_oops/getter_setter.js
@@ -3,6 +3,27 @@ class User{
         this.email=email;
         this.password=password
     }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(val){
+       this._email=val.toUpperCase();
+    }
+    get password(){
+      return `${this._password}ari`;
+    }
+    set password(val){
+        this._password=val.toUpperCase();
+    }
+
 }
-const user=new User("sam","134121");
-console.log(user);
+const user=new User("sam","123sam");
+//console.log(user);
+// console.log(user.password)
+// console.log(user.email)
+user.password='pass';
+user.email="userEmail@123";
+console.log(user.password)
+console.log(user.email)
+
+
